0a90a7c492752ade2f6c2b32dc33d2379fafb3a3
hayato.fujimori <shangrilasars6388@gmail.com>
2021-05-13 02:41:28 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
+            
+            {!! link_to_route('singup.get','sing up now!',[],['class' => 'btn btn-primary']) !!}
         </div>
     </div>
 @endsection
