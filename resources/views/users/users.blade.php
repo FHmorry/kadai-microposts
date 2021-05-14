8a234fd5f9ffd52b003e4ef28499fac13930c8de
hayato.fujimori <shangrilasars6388@gmail.com>
2021-05-14 02:32:45 +0000

    user pages

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -2,13 +2,14 @@
     <ul class="list-unstyled">
         @foreach ($users as $user)
             <li class="media">
-                <img class="mr-2 rounded" src="{{ Gravatar::get($user->emal,['size' => 50]) }}" alt="">
+                <img class="mr-2 rounded" src="{{ Gravatar::get($user->email,['size' => 50]) }}" alt="">
+                
                 <div class="media-body">
                     <div>
                         {{ $user->name }}
                     </div>
                     <div>
-                        <p>{!! link_to_route('users.show','view profile',['user'->id]) !!}</p>
+                        <p>{!! link_to_route('users.show','view profile',['user' => $user->id]) !!}</p>
                     </div>
                 </div>
             </li>
