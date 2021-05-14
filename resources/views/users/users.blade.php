3c1cfa68cf410866f834c04ebca19309b1c9e1cf
hayato.fujimori <shangrilasars6388@gmail.com>
2021-05-14 02:28:39 +0000

    user pages

---
+++ resources/views/users/users.blade.php
@@ -0,0 +1,20 @@
+@if(count($users) > 0)
+    <ul class="list-unstyled">
+        @foreach ($users as $user)
+            <li class="media">
+                <img class="mr-2 rounded" src="{{ Gravatar::get($user->emal,['size' => 50]) }}" alt="">
+                <div class="media-body">
+                    <div>
+                        {{ $user->name }}
+                    </div>
+                    <div>
+                        <p>{!! link_to_route('users.show','view profile',['user'->id]) !!}</p>
+                    </div>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+    
+    {{ $users->links() }}
+    
+@endif
