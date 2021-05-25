c116189e52571878e959c615f12b78f0e8eb1b42
hayato.fujimori <shangrilasars6388@gmail.com>
2021-05-25 02:05:20 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -113,7 +113,7 @@ class User extends Authenticatable
     
     public function feed_microposts()
     {
-        $userIds = $this->followings()->pluck('uses.id')->toArray();
+        $userIds = $this->followings()->pluck('users.id')->toArray();
         
         $userIds[] = $this->id;
         
